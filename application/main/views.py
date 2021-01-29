0ff00926c45d7a98187c7bd3037d64c6ad204d9d
Sebastian Fernandez <seba.fernandez95@gmail.com>
2021-01-28 22:58:06 -0300

    Added OMDB API to make API Calls

--- application/main/views.py
+++ application/main/views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, url_for, render_template, request, redirect, session
 from application.models import User
 from application.extensions import db
+from operator import itemgetter
+import requests
 
 main = Blueprint('main', __name__)
 
@@ -15,8 +17,40 @@ def index ():
         information = User.query.all()
         
         return render_template ("index.html", information=information)
-    
 
+@main.route ('/search', methods = ["GET", "POST"])
+def search ():
+    if request.method == "POST":
+        user_search = request.form.get ("name").replace (' ', '+')
+        
+        return redirect (url_for('main.search_results', name = user_search))
+    
+    return render_template ("search.html")
+
+@main.route ('/search/<name>')
+def search_results(name):
+    URL = 'https://www.omdbapi.com/?s=' + name + '&apikey=678cd26e'
+    
+    q = requests.get(url=URL)
+    r = q.json()
+        
+    d = {}
+    dlist = []
+        
+    if q.status_code == 200:
+        if r['Response'] == 'True':
+            for i in r['Search']:
+                d = {
+                    'Title' : i.get ('Title'),
+                    'Year' : i.get ('Year')
+                }
+                    
+                dlist.append (d)
+                
+            rlist = sorted (dlist, key=itemgetter ('Title'), reverse=False)
+            
+    return render_template ("search.html", table=True, list = rlist)
+    
 @main.route ('/login')
 def login ():
     session['loged'] = True
