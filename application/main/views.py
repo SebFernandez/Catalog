8f37ef6e682648425919eeb9b7ad660a904b13d2
Sebastian Fernandez <seba.fernandez95@gmail.com>
2021-02-02 20:00:18 -0300

    Fixed UI

--- application/main/views.py
+++ application/main/views.py
@@ -10,7 +10,7 @@ main = Blueprint('main', __name__)
 
 #TODO
     - Mark many items to add them in group, also to delete them.
-    - Unit test
+    - Unit test.
     
 '''
 @main.route ('/', methods = ["GET", "POST"])
@@ -45,84 +45,89 @@ def index ():
 
 @main.route ('/search', methods = ["GET", "POST"])
 def search ():
-    if request.method == "POST":
-        user_search = request.form.get ("name").replace (' ', '+')
-        
-        return redirect (url_for('main.search_results', name = user_search))
+    if 'loged' in session:
+        if request.method == "POST":
+            user_search = request.form.get ("name").replace (' ', '+')
+            
+            return redirect (url_for('main.search_results', name = user_search))
     
-    return render_template ("search.html")
+        return render_template ("search.html", session = True)
+    
+    return redirect (url_for ('main.index'))
 
 @main.route ('/search/<name>',  methods = ["GET", "POST"])
 def search_results(name):
-    d = {}
-    
-    if request.method == "GET":
-        URL = 'https://www.omdbapi.com/?s=' + name + '&apikey=678cd26e'
+    if 'loged' in session:
+        d = {}
         
-        q = requests.get(url=URL)
-        r = q.json()
-            
-        dlist = []
-            
-        if q.status_code == 200:
-            if r['Response'] == 'True':
-                for i in r['Search']:
-                    d = {
-                        'Title' : i.get ('Title'),
-                        'Year' : i.get ('Year'),
-                        'Type': i.get ('Type').capitalize(),
-                    }
-                        
-                    dlist.append (d)
-                
-                #! Param 'key' needs the key to sort.            
-                rlist = sorted (dlist, key=itemgetter ('Title'), reverse=False)
-                
-                return render_template ("search.html", table=True, list = rlist)
-        
-        return redirect(url_for('main.search'))
-    
-    elif request.method == "POST":
-        title = request.form.get ("title")
-        
-        if not bool(Content.query.filter_by(name=title).first()):
-            
-            URL = 'https://www.omdbapi.com/?t=' + title + '&apikey=678cd26e'
+        if request.method == "GET":
+            URL = 'https://www.omdbapi.com/?s=' + name + '&apikey=678cd26e'
             
             q = requests.get(url=URL)
             r = q.json()
-            
+                
+            dlist = []
+                
             if q.status_code == 200:
                 if r['Response'] == 'True':
-                    d = {
-                        'Title': r['Title'],
-                        'Year' : r['Year'],
-                        'Genre': r['Genre'],
-                        'Rating' : float (r['imdbRating']),
-                        'Type': r['Type'].capitalize()
-                    }
-        
-            new_content = Content (name=d['Title'], year=d['Year'], genre=d['Genre'], rating = d['Rating'], content_type= d['Type'])
+                    for i in r['Search']:
+                        d = {
+                            'Title' : i.get ('Title'),
+                            'Year' : i.get ('Year'),
+                            'Type': i.get ('Type').capitalize(),
+                        }
+                            
+                        dlist.append (d)
+                    
+                    #! Param 'key' needs the key to sort.            
+                    rlist = sorted (dlist, key=itemgetter ('Title'), reverse=False)
+                    
+                    return render_template ("search.html", session = True, table=True, list = rlist)
             
-            db.session.add (new_content)
-            db.session.commit ()
+            return redirect(url_for('main.search'))
         
-        return redirect (url_for("main.index"))
+        elif request.method == "POST":
+            title = request.form.get ("title")
+            
+            if not bool(Content.query.filter_by(name=title).first()):
+                
+                URL = 'https://www.omdbapi.com/?t=' + title + '&apikey=678cd26e'
+                
+                q = requests.get(url=URL)
+                r = q.json()
+                
+                if q.status_code == 200:
+                    if r['Response'] == 'True':
+                        d = {
+                            'Title': r['Title'],
+                            'Year' : r['Year'],
+                            'Genre': r['Genre'],
+                            'Rating' : float (r['imdbRating']),
+                            'Type': r['Type'].capitalize()
+                        }
+            
+                new_content = Content (name=d['Title'], year=d['Year'], genre=d['Genre'], rating = d['Rating'], content_type= d['Type'])
+                
+                db.session.add (new_content)
+                db.session.commit ()
+            
+    return redirect (url_for("main.index"))
 
 @main.route ('/edit/<content_name>', methods = ["GET", "POST"])
 def edit (content_name):
-    content = Content.query.filter_by(name=content_name).first()
-    
-    if request.method == "GET":
-        return render_template ("index.html", edit = True, name = content.name,)
-    
-    elif request.method == "POST":
-        if request.form.get ('your-rating'):
-            content.user_rating = request.form.get ('your-rating')
+    if 'loged' in session:
+        content = Content.query.filter_by(name=content_name).first()
         
-            db.session.commit ()
+        if request.method == "GET":
+            return render_template ("index.html", session = True, edit = True, content = content)
+        
+        elif request.method == "POST":
+            if request.form.get ('your-rating'):
+                content.user_rating = request.form.get ('your-rating')
+            
+                db.session.commit ()
     
-        return redirect (url_for ('main.index'))
+    return redirect (url_for ('main.index'))
    
 @main.route ('/login')
 def login ():
